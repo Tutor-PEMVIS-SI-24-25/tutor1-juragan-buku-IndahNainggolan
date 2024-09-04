a0febb9b0fee90dd1c784f7cb8bbd35f7bb813dc
IndahNainggolan <indahnnyxt@gmail.com>
2024-09-04 21:27:07 +0700

    telah menyelesaiakan tugss

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24046 - Indah Elsadai Nainggolan
+// 12S24046 - Indah Elsadai Nainggolan
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println("yahahaha putus");
+    }
+}
